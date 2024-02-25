bb40c6dcaf97107d95b1a7182c92e1e60be9c1fc
Gregory Bouriez <gregbouriez@hotmail.com>
2024-02-25 11:12:16 +0100

    button modifier et supprimer

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 const tasklist = document.getElementById('taskList');
+const inputTask = document.getElementById('inputTask');
+
 
 function addTask(){
-  const inputTask = document.getElementById('inputTask');
-  const inputText = inputTask.value;
+  //créer une tâche
+  let inputText = inputTask.value;
+
+  if(inputText ===""){
+    return;
+  }
+
   let li = document.createElement('li');
   li.innerHTML = inputText;
   tasklist.appendChild(li);
 
-if(inputText ===""){
-  return;
-}
+
+  //modifier une tâche
+  const updateLi = document.createElement('button');
+  updateLi.innerHTML = 'Modifier';
+  updateLi.onclick = function(){
+    modifyTask()
+  };
+
+  //supprimer une tâche
+  const deleteLi = document.createElement('button');
+  deleteLi.innerHTML = 'Supprimer';
+  deleteLi.onclick = function (){
+    deleteTask()
+  };
+
+
+  li.appendChild(updateLi);
+  li.appendChild(deleteLi);
+  inputTask.value = "";
 }
 
+//modifié la saisie
+function modifyTask(){
+  
+}
+
+
+//supprimer la saisie
+
+function deleteTask(){}
